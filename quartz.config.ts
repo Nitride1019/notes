34caaea938e72b755ae4b54b146ae4eae94fbe4c
LC Lin <lclin1019@gmail.com>
2024-08-01 21:12:27 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,6 @@ const config: QuartzConfig = {
   },
   plugins: {
     transformers: [
-      Plugin.Latex({ renderEngine: "katex" }),
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"],
@@ -71,7 +70,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex({ renderEngine: "math-jax" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
